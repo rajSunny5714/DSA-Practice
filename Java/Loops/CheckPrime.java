842d04857aa7f4e77855ef9143f5d89a867bd94c
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2026-01-05 10:00:41 +0530

    Add files via upload

---
+++ Java/Loops/CheckPrime.java
@@ -0,0 +1,19 @@
+package Loops;
+import java.util.*;
+public class CheckPrime{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<=1){
+            System.out.println("Not Prime");
+            return;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                System.out.println("Not Prime");
+                return;
+            }
+        }
+        System.out.println("Prime");
+    }
+}
---
+++ Java/Loops/PrimeNum.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.*;
+public class PrimeNum{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=2;i<=n;i++){
+            boolean prime=true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    prime=false;
+                    break;
+                }
+            }
+            if(prime) System.out.print(i+" ");
+        }
+    }
+}
